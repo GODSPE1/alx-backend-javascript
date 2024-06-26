cb697743b2334211fb966a8cac4fe7034d71686f
GODSPE1 <godspy18@gmail.com>
2024-06-27 00:31:42 +0100

    corrected mistakes

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,3 +1,12 @@
-function handleProfileSignup() {
+import { uploadPhoto, createUser } from './utils';
 
+function handleProfileSignup() {
+  return new Promise.all([uploadPhoto(), createUser()])
+    .then((responses) => {
+      const [photoResponse, userResponse] = responses;
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
