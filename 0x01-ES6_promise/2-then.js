b2c5fc72220eba6b094ad0262ef656c80da9edd0
GODSPE1 <godspy18@gmail.com>
2024-06-26 16:25:49 +0100

    added three handlers

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,10 @@
+function handleResponseFromAPI(promise) {
+  return new Promise((resolve, reject) => {
+    if (promise) {
+      resolve({ status: 200, body: 'success' });
+    } else {
+      reject(Error);
+    }
+    console.log('Got a response from the API');
+  });
+}
