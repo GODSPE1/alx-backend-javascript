5cd08b8a2b268c8ffe2b379f3b7f377242556a05
GODSPE1 <godspy18@gmail.com>
2024-06-27 10:14:37 +0100

    updated the function

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,9 @@
-function getResponseFromAPI(args) {
+export default function getResponseFromAPI(args) {
   return new Promise((resolve, reject) => {
     if (args === true) {
-      resolve(console.log({ status: 200, body: 'Success' }));
+      resolve(({ status: 200, body: 'Success' }));
     } else {
-      reject(Error('The fake API is not working currently'));
+      reject(new Error('The fake API is not working currently'));
     }
   });
 }
