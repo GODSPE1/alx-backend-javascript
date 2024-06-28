8dfe7d8c29364bc55634f32a5529843ae371b6ba
GODSPE1 <godspy18@gmail.com>
2024-06-28 14:55:44 +0100

    correct to add floors to it

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,20 +1,21 @@
-import Building from "./5-building";
+import Building from './5-building';
 
-class SkyHighBuilding extends  Building {
-    constructor(sqft, floors) {
-        super(sqft);
-        this._floors = floors
-    }
-    get sqft() {
-        return this._sqft;
-    }
+class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
 
-    get floors() {
-        return (this._floors);
-    }
+  get sqft() {
+    return this._sqft;
+  }
 
-    evacuationWarningMessage () {
-        return ('Evacuate slowly the NUMBER_OF_FLOORS floors');
-    }
+  get floors() {
+    return (this._floors);
+  }
+
+  evacuationWarningMessage() {
+    return (`Evacuate slowly the ${this.floors} floors`);
+  }
 }
 export default SkyHighBuilding;
