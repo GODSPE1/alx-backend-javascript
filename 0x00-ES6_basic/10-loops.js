0f2ce6c0a60a7b4ec0ec6b6eee85521887fe9054
GODSPE1 <godspy18@gmail.com>
2024-06-29 01:11:31 +0100

    clean working tree

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-    for (const [idx, value] of array.entries()) {
-      array[idx] = array[idx] + appendString;
-    }
-  
-    return array;
+  const arr = []
+  for (const [idx, value] of array.entries()) {
+    const value = array[idx];
+    array[idx] = appendString + value;
   }
+
+  return array;
+}
