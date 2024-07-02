4cb683c06fee00d9e25b0b57f09694dd0bb398ab
GODSPE1 <godspy18@gmail.com>
2024-07-02 06:12:28 +0100

    function that returns an object

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,21 @@
+export default function getListStudents() {
+  const myObject = [
+    {
+      id: 1,
+      firstName: 'Gulaume',
+      location: 'Sanfrancisco',
+    },
+    {
+      id: 2,
+      firstName: 'James',
+      location: 'Columbia',
+    },
+    {
+      id: 5,
+      firstName: 'Serena',
+      location: 'San francisco',
+    },
+
+  ];
+  return myObject;
+}
