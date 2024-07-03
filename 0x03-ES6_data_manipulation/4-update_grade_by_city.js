625e04910d4d075acc89f609ccacabd30a50aa63
GODSPE1 <godspy18@gmail.com>
2024-07-03 04:35:04 +0100

    correct linting error

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,16 +1,16 @@
 export default function updateStudentGradeByCity(studentList, city, newGrades) {
-    const students = studentList.filter((student) => student.location == city);
-    for (const student of students) {
-        student.grade == "N/A";
+  const students = studentList.filter((student) => student.location === city);
+  for (const student of students) {
+    student.grade = 'N/A';
+  }
+  const newStudents = students.map((student) => {
+    const single = student;
+    for (const grade of newGrades) {
+      if (single.id === grade.studentId) {
+        single.grade = grade.grade;
+      }
     }
-    const newStudents = students.map((student) => {
-        const single = student;
-        for (const grade of newGrades) {
-            if (single.id === grade.studentId) {
-                single.grade = grade.grade;
-            }
-        }
-        return single;
-    })
-    return newStudents;
+    return single;
+  });
+  return newStudents;
 }
