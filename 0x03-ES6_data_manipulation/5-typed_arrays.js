b4ee11c1858628c48bbd83cde75ffacc71e567a3
GODSPE1 <godspy18@gmail.com>
2024-07-03 04:23:28 +0100

    returns a new ArrayBuffer with an Int8 value at a specific position.

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,1 +1,11 @@
-to
+export default function createInt8TypedArray(length, position, value) {
+    const arr = new  ArrayBuffer(length);
+    const val = new  DataView(arr);
+
+    try {
+        val.setInt8(position, value);
+    } catch (e) {
+        throw Error('Position outside rand')
+    }
+  return val;
+}
