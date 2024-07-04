d1f25fbf9cf5afb8cabc432a9c51d3816917698b
GODSPE1 <godspy18@gmail.com>
2024-07-04 02:26:46 +0100

    task 4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -17,3 +17,30 @@ type printTeacherFunction = (firstName: string, lastName: string) => string;
 const printTeacher: printTeacherFunction = (firstName, lastName) => {
     return `${firstName[0]}. ${lastName}`;
 };
+
+
+interface StudentClassInterface {
+    firstName: string;
+    lastName: string;
+}
+
+class StudentClass implements StudentClassInterface {
+    firstName: string;
+    lastName: string;
+    
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+
+    workOnHomework(): string {
+      return 'Currently working';
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
+
+
