984f4d05890929e0af113e2962b46639cf9ef756
GODSPE1 <godspy18@gmail.com>
2024-07-04 16:30:44 +0100

    returns the addition of two float numbers

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,2 +1,8 @@
-def add(a: int, b):
-    return a + b
+#!/usr/bin/env python3
+"""This module defines annotation of float function
+"""
+
+
+def add(a: float, b: float) -> float:
+        """This funtion return the addition of two float numbers"""
+        return a + b
