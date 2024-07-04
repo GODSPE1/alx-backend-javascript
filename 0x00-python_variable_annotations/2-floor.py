b7f17d174dcd6a40dfd27dc0b432081e23c14347
GODSPE1 <godspy18@gmail.com>
2024-07-04 18:01:53 +0100

    type-annotated function floor which takes a float n as argument and returns the floor of the float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This module defines a function that returns the rounddown of a number
+"""
+
+
+def floor(n: float) -> int:
+    """function returns the rounded down of a number"""
+    return int(n // 1)
