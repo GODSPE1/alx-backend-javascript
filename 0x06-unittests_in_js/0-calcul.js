a93ffafe782768819ba2b6583f26e81e27443329
GODSPE1 <godspy18@gmail.com>
2024-08-23 04:35:14 +0100

    remove space

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,5 +1,6 @@
-function calculateNumber (a, b) {
+function calculateNumber(a, b) {
     return Math.round(a)  + Math.round(b);
-}
+};
 
+// console.log(calculateNumber(1.6, 1.7))
 module.exports = calculateNumber;
